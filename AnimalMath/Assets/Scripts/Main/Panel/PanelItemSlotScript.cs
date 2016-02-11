01616ede81aafd6e8d8a24e4a9def0a593826afd
LeeWonJoo <leewonjoo@LeeWonJooui-MacBook-Pro.local>
2016-02-12 00:47:23 +0900

    아이템 슬롯 선택 적용. 이미지 추후 적용 예정.

--- AnimalMath/Assets/Scripts/Main/Panel/PanelItemSlotScript.cs
+++ AnimalMath/Assets/Scripts/Main/Panel/PanelItemSlotScript.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 
 public class PanelItemSlotScript : PanelBaseScript {
 
+	public delegate void OnSelected (string name);
+	public static event OnSelected OnEffectSelected;
+	public static event OnSelected OnMathSelected;
+	public static event OnSelected OnPassiveSelected;
+
+
 	private Dictionary<string, Transform> dicEffect = new Dictionary<string, Transform>();
 	private Dictionary<string, Transform> dicMath = new Dictionary<string, Transform>();
 	private Dictionary<string, Transform> dicPassive = new Dictionary<string, Transform>();
-
-
 
 	void Awake()
 	{
@@ -18,87 +22,93 @@ public class PanelItemSlotScript : PanelBaseScript {
 	public override void OnInit ()
 	{
 		Debug.Log("OnInit");
-		GameData.SetBtn(this.transform, "Effect1", "Press", this);
-		GameData.SetBtn(this.transform, "Effect2", "Press", this);
-		GameData.SetBtn(this.transform, "Effect3", "Press", this);
-		GameData.SetBtn(this.transform, "MathEffect1", "Press", this);
-		GameData.SetBtn(this.transform, "MathEffect2", "Press", this);
-		GameData.SetBtn(this.transform, "MathEffect3", "Press", this);
-		GameData.SetBtn(this.transform, "Passive1", "Press", this);
-		GameData.SetBtn(this.transform, "Passive2", "Press", this);
-		GameData.SetBtn(this.transform, "Passive3", "Press", this);
 
-		dicEffect.Add("Effect1" , this.transform.FindChild("Effect1"));
-		dicEffect.Add("Effect2" , this.transform.FindChild("Effect2"));
-		dicEffect.Add("Effect3" ,this.transform.FindChild("Effect3"));
-		dicMath.Add("MathEffect1", this.transform.FindChild("MathEffect1"));
-		dicMath.Add("MathEffect2", this.transform.FindChild("MathEffect2"));
-		dicMath.Add("MathEffect3", this.transform.FindChild("MathEffect3"));
-		dicPassive.Add("Passive1", this.transform.FindChild("Passive1"));
-		dicPassive.Add("Passive2", this.transform.FindChild("Passive2"));
-		dicPassive.Add("Passive3", this.transform.FindChild("Passive3"));
+		string sEffect = "";
+		string sMath = "";
+		string sPassive = "";
 
+		for (int i = 0; i < 3; i++) {
+			sEffect = string.Format("Effect{0}",i+1);
+			sMath = string.Format("MathEffect{0}",i+1);
+			sPassive = string.Format("Passive{0}",i+1);
+			Debug.Log(sEffect);
+			GameData.SetBtn(this.transform, sEffect, "Press", this);
+			GameData.SetBtn(this.transform, sMath, "Press", this);
+			GameData.SetBtn(this.transform, sPassive, "Press", this);
+			dicEffect.Add(sEffect , this.transform.FindChild(sEffect));
+			dicMath.Add(sMath, this.transform.FindChild(sMath));
+			dicPassive.Add(sPassive, this.transform.FindChild(sPassive));
+		}
 	}
 
 	public override void OnPress(GameObject oBtn)
 	{
 		if(oBtn.name == "Effect1"){
 			Debug.Log("Effect1");
-			SetButton(oBtn.name);
+			SetEffectBtton(oBtn.name);
 		} else if(oBtn.name == "Effect2"){
 			Debug.Log("Effect2");
-			SetButton(oBtn.name);
+			SetEffectBtton(oBtn.name);
 		} else if(oBtn.name == "Effect3"){
 			Debug.Log("Effect3");
-			SetButton(oBtn.name);
+			SetEffectBtton(oBtn.name);
 		} else if(oBtn.name == "MathEffect1"){
 			Debug.Log("MathEffect1");
-			SetButton(oBtn.name);
+			SetMathBtton(oBtn.name);
 		} else if(oBtn.name == "MathEffect2"){
 			Debug.Log("MathEffect2");
-			SetButton(oBtn.name);
+			SetMathBtton(oBtn.name);
 		} else if(oBtn.name == "MathEffect3"){
 			Debug.Log("MathEffect3");
-			SetButton(oBtn.name);
+			SetMathBtton(oBtn.name);
 		} else if(oBtn.name == "Passive1"){
 			Debug.Log("Passive1");
-			SetButton(oBtn.name);
+			SetPassiveButton(oBtn.name);
 		} else if(oBtn.name == "Passive2"){
 			Debug.Log("Passive2");
-			SetButton(oBtn.name);
+			SetPassiveButton(oBtn.name);
 		} else if(oBtn.name == "Passive3"){
 			Debug.Log("Passive3");
-			SetButton(oBtn.name);
+			SetPassiveButton(oBtn.name);
 		}
 
 	}
 
-	void SetButton(string name)
+	void SetEffectBtton(string name)
 	{
 		if(dicEffect[name] != null){
-			
 			foreach (Transform item in dicEffect.Values) {
 				item.GetComponent<UISprite>().spriteName = "BtnGreen";
 			}
 			this.transform.FindChild(name).GetComponent<UISprite>().spriteName = "BtnRed";
-
+			OnEffectSelected(name);
 		}
-//		if(dicMath[name] != null){
-//			foreach (Transform item in dicMath.Values) {
-//				item.GetComponent<UISprite>().spriteName = "BtnGreen";
-//			}
-//			this.transform.FindChild(name).GetComponent<UISprite>().spriteName = "BtnRed";
-//			
-//		}
-//		if(dicPassive[name] != null){
-//			foreach (Transform item in dicPassive.Values) {
-//				item.GetComponent<UISprite>().spriteName = "BtnGreen";
-//			}
-//			this.transform.FindChild(name).GetComponent<UISprite>().spriteName = "BtnRed";
-//		}
-
 	}
 
+	void SetMathBtton(string name)
+	{
+		if(dicMath[name] != null){
+
+			foreach (Transform item in dicMath.Values) {
+				item.GetComponent<UISprite>().spriteName = "BtnGreen";
+			}
+			this.transform.FindChild(name).GetComponent<UISprite>().spriteName = "BtnRed";
+			OnMathSelected(name);
+		}
+	}
+
+	void SetPassiveButton(string name)
+	{
+		if(dicPassive[name] != null){
+
+			foreach (Transform item in dicPassive.Values) {
+				item.GetComponent<UISprite>().spriteName = "BtnGreen";
+			}
+			this.transform.FindChild(name).GetComponent<UISprite>().spriteName = "BtnRed";
+			OnPassiveSelected(name);
+		}
+	}
+		
 
 	void Start () {
 		
--- AnimalMath/Assets/Scripts/Main/Panel/PanelReadyScript.cs
+++ AnimalMath/Assets/Scripts/Main/Panel/PanelReadyScript.cs
@@ -5,7 +5,13 @@ using UnityEditor.SceneManagement;
 
 public class PanelReadyScript : PanelBaseScript
 {
+	void Awake()
+	{
+		PanelItemSlotScript.OnEffectSelected += OnEffectSelected;
+		PanelItemSlotScript.OnMathSelected += OnMathSelected;
+		PanelItemSlotScript.OnPassiveSelected += OnPassiveSelected;
 
+	}
 	public override void OnInit ()
 	{
 		GameData.SetBtn (this.transform, "BtnEasy", "Press", this);
@@ -17,8 +23,9 @@ public class PanelReadyScript : PanelBaseScript
 		GameData.SetBtn (this.transform, "SlotEffect", "Press", this);
 		GameData.SetBtn (this.transform, "SlotMath", "Press", this);
 		GameData.SetBtn (this.transform, "SlotPassive", "Press", this);
-	}
 
+	}
+		
 	public override void OnPress (GameObject oBtn)
 	{
 		if (oBtn.name == "BtnEasy") {
@@ -39,9 +46,22 @@ public class PanelReadyScript : PanelBaseScript
 		} else if (oBtn.name == "SlotPassive"){
 			Debug.Log("Passive");
 		}
-
 	}
 
+	void OnEffectSelected(string name)
+	{
+		Debug.Log(name);
+		this.transform.FindChild("SlotEffect").GetComponentInChildren<UILabel>().text = name;
+
+	}
+	void OnMathSelected(string name)
+	{
+		this.transform.FindChild("SlotMath").GetComponentInChildren<UILabel>().text = name;
+	}
+	void OnPassiveSelected(string name)
+	{
+		this.transform.FindChild("SlotPassive").GetComponentInChildren<UILabel>().text = name;
+	}
 	public override void OnExit ()
 	{
 		
