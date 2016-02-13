bd417756cb525c86dd3a8ce85d96b29c70ebe377
LeeWonJoo <leewonjoo@LeeWonJooui-MacBook-Pro.local>
2016-02-13 20:29:46 +0900

    no message

--- AnimalMath/Assets/Scripts/Main/Panel/PanelMainScript.cs
+++ AnimalMath/Assets/Scripts/Main/Panel/PanelMainScript.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class PanelMainScript : PanelBaseScript {
 
+	Transform oBG;
+	Transform oEffect;
+
 	// Use this for initialization
 	public override void OnInit ()
 	{
+		oBG = this.transform.FindChild("BtnOption").FindChild("BG") as Transform;
+		oEffect = this.transform.FindChild("BtnOption").FindChild("Effect") as Transform;
 		GameData.SetBtn(this.transform, "BtnGameReady","Press", this);
 		GameData.SetBtn(this.transform, "BtnOption", "Press", this);
 	}
@@ -17,13 +22,25 @@ public class PanelMainScript : PanelBaseScript {
 		if (oBtn.name == "BtnGameReady") {
 			m_sManager.SetScene(SceneState.GameReady);
 		} else if (oBtn.name == "BtnOption") {
-			
+			onOption();
 		}
 	}
 	void onOption()
 	{
-		
+		StartCoroutine(openOption);
 	}
+
+	IEnumerator openOption()
+	{
+		oBG.transform.position = new Vector3(oBG.transform.position.x - 50,
+			oBG.transform.position.y,
+			oBG.transform.position.z);
+		oEffect.transform.position = new Vector3(oEffect.transform.position.x - 25,
+			oEffect.transform.position.y,
+			oEffect.transform.position.z);
+		yield return new WaitForSeconds(0.5f);
+	}
+
 	// Update is called once per frame
 	void Update () {
 	}
