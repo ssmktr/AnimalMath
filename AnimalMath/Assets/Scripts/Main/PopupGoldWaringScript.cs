2801235e8cb7ead9708ddeda35bec0b5eb0cd768
이원주 <forrs2@lee-ui-MacBook-Pro.local>
2016-02-24 23:31:22 +0900

    골드 스킬 데이터 저장 적용. 골드 부족시 팝업창.

---
+++ AnimalMath/Assets/Scripts/Main/PopupGoldWaringScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupGoldWaringScript : MonoBehaviour {
+
+
+	void Awake(){
+		GameData.SetBtn (this.transform, "BtnOk", "Press", this);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Press(GameObject obj)
+	{
+		if (obj.name == "BtnOk") {
+			Debug.Log ("name");
+			Destroy (this.gameObject);
+		}
+
+	}
+
+}
--- AnimalMath/Assets/Scripts/Panel/PanelReadyScript.cs
+++ AnimalMath/Assets/Scripts/Panel/PanelReadyScript.cs
@@ -9,6 +9,12 @@ public class PanelReadyScript : PanelBaseScript
 	private SkillState m_eMath = SkillState.None;
 	private SkillState m_ePassive = SkillState.None;
 	private GameObject m_oSkillControl;
+	private GameObject POPUP;
+	private UILabel m_lbGold;
+	private int m_nTempPrice = 0;
+	private int m_nEffectPrice = 0;
+	private int m_nMathPrice = 0;
+	private int m_nPassivePrice = 0;
 
 	public override void OnInit () {
 		GameData.SetBtn (this.transform, "BtnBack", "Press", this);
@@ -20,6 +26,11 @@ public class PanelReadyScript : PanelBaseScript
 		GameData.SetBtn (this.transform, "ViewMath", "Press", this);
 		GameData.SetBtn (this.transform, "ViewPassive", "Press", this);
 
+		POPUP = (GameObject)Resources.Load ("Prefabs/Popup/Goldwaring");
+		m_lbGold = this.transform.FindChild ("Gold").FindChild ("Label").GetComponent<UILabel> ();
+		InitGold ();
+		SetGoldText();
+
 		m_oSkillControl = this.transform.FindChild ("SkillControl").gameObject;
 		SetAllSkill ();
 		SetStageLevel ();
@@ -29,18 +40,86 @@ public class PanelReadyScript : PanelBaseScript
 			GameObject oSkill = m_oSkillControl.transform.GetChild (i).gameObject;
 			oSkill.GetComponent<UISprite> ().color = Color.white;
 			oSkill.GetComponent<UIEventTrigger> ().onClick = GameData.SetOnClick (this, "Press", oSkill);
+			oSkill.transform.FindChild ("Sprite").FindChild ("Price").GetComponent<UILabel> ().text = DataManager.Instance.AllSkillData [i].Price.ToString();
 		}
 		SetEffectSkill ();
 		SetMathSkill ();
 		SetPassiveSkill ();
 		ViewSkill ();
 	}
+	void InitGold()
+	{
+		m_nTempPrice = GameManager.Instance.Gold;
+	}
+	void SetTotalGold()
+	{
+		GameManager.Instance.Gold = m_nTempPrice + m_nEffectPrice + m_nMathPrice + m_nPassivePrice;
+	}
 
+	bool HasEnoughGold(SkillState ef, SkillState ma, SkillState pa)
+	{
+		int effectPrice=0;
+		int mathPrice=0;
+		int passivePrice=0;
+
+		if (SkillState.None != ef) {
+			effectPrice = DataManager.Instance.AllSkillData [(int)ef].Price;
+		}
+		if (SkillState.None != ma) {
+			mathPrice = DataManager.Instance.AllSkillData [(int)ma].Price;
+		}
+		if (SkillState.None != pa) {
+			passivePrice = DataManager.Instance.AllSkillData [(int)pa].Price;	
+		}
+		int totalValue = m_nTempPrice - (effectPrice + mathPrice + passivePrice); 			
+		if (0 > totalValue) {
+			//사려는 금액보다 가지고 있는 돈의 액수가 적을경우 
+			Debug.Log("Not Enough Momey");
+			SetPopup ();
+			return false;
+		}
+		return true;
+	}
+	void SetPopup()
+	{
+		GameObject popup = Instantiate (POPUP);
+		popup.name = "goldwaring";
+		popup.transform.parent = this.transform;
+		popup.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+		popup.transform.localScale = Vector3.one;
+//		popup.transform.FindChild ("ok").GetComponent<UIEventTrigger> ().onClick = GameData.SetOnClick (this, "Press", this);
+	}
+	void SetGoldText()
+	{
+		m_lbGold.text = @"Gold : " + GameManager.Instance.Gold.ToString ();
+	}
+
+	void ResetGold()
+	{
+		m_nEffectPrice 	= 0;
+		m_nMathPrice 	= 0;
+		m_nPassivePrice = 0;
+		GameManager.Instance.Gold = m_nTempPrice;
+		SetGoldText ();
+	}
+	void ResetItem()
+	{
+		m_eEffect 	= SkillState.None;
+		m_eMath 	= SkillState.None;
+		m_ePassive 	= SkillState.None;
+	}
 	void SetEffectSkill(){
 		if (SkillState.None != m_eEffect) {
 			int idx = (int)m_eEffect;
 			GameObject oSkill = m_oSkillControl.transform.GetChild (idx).gameObject;
 			oSkill.GetComponent<UISprite> ().color = Color.blue;
+			m_nEffectPrice = - DataManager.Instance.AllSkillData[idx].Price;
+			SetTotalGold ();
+			SetGoldText ();
+		} else if (SkillState.None == m_eEffect) {
+			m_nEffectPrice = 0;
+			SetTotalGold ();
+			SetGoldText ();
 		}
 	}
 	void SetMathSkill(){
@@ -48,6 +127,13 @@ public class PanelReadyScript : PanelBaseScript
 			int idx = (int)m_eMath;
 			GameObject oSkill = m_oSkillControl.transform.GetChild (idx).gameObject;
 			oSkill.GetComponent<UISprite> ().color = Color.green;
+			m_nMathPrice = - DataManager.Instance.AllSkillData [idx].Price;
+			SetTotalGold ();
+			SetGoldText ();
+		} else if (SkillState.None == m_eMath) {
+			m_nMathPrice = 0;
+			SetTotalGold ();
+			SetGoldText ();
 		}
 	}
 	void SetPassiveSkill(){
@@ -55,6 +141,13 @@ public class PanelReadyScript : PanelBaseScript
 			int idx = (int)m_ePassive;
 			GameObject oSkill = m_oSkillControl.transform.GetChild (idx).gameObject;
 			oSkill.GetComponent<UISprite> ().color = Color.red;
+			m_nPassivePrice = - DataManager.Instance.AllSkillData [idx].Price;
+			SetTotalGold ();
+			SetGoldText ();
+		} else if (SkillState.None == m_ePassive) {
+			m_nPassivePrice = 0;
+			SetTotalGold ();
+			SetGoldText ();
 		}
 	}
 	void SetStageLevel(){
@@ -91,6 +184,9 @@ public class PanelReadyScript : PanelBaseScript
 		GameManager.Instance.playerData.Effect.Name = m_eEffect;
 		GameManager.Instance.playerData.Math.Name = m_eMath;
 		GameManager.Instance.playerData.Passive.Name = m_ePassive;
+		int value = GameManager.Instance.Gold;
+		Debug.Log ("Will Save Gold" + value);
+		GameManager.Instance.SaveGameDataToLocal ();
 	}
 	public override void OnPress (GameObject oBtn)
 	{
@@ -104,6 +200,8 @@ public class PanelReadyScript : PanelBaseScript
 			InGameData ();
 			AdsData.PlayAds ();
 		} else if (oBtn.name == "BtnBack") {
+			ResetGold ();
+			ResetItem ();
 			m_sManager.SetScene (SceneState.Main);
 		} else if (oBtn.name == "ViewEffect") {
 			m_eEffect = SkillState.None;
@@ -114,15 +212,21 @@ public class PanelReadyScript : PanelBaseScript
 		} else if (oBtn.name.Contains ("Skill")) {
 			int idx = int.Parse (oBtn.name.Replace ("Skill", ""));
 			if (3 > idx) {
-				m_eEffect = (SkillState)idx;
+				if (HasEnoughGold((SkillState)idx, m_eMath, m_ePassive)) {
+					m_eEffect = (SkillState)idx;	
+				}
 			} else if (5 < idx) {
-				m_ePassive = (SkillState)idx;
+				if (HasEnoughGold(m_eEffect, m_eMath, (SkillState)idx)) {
+					m_ePassive = (SkillState)idx;	
+				}
 			} else {
-				m_eMath = (SkillState)idx;
+				if (HasEnoughGold(m_eEffect, (SkillState)idx, m_ePassive)) {
+					m_eMath = (SkillState)idx;
+				}
 			}
 		}
 		SetStageLevel ();
-		SetAllSkill ();
+		SetAllSkill ();	
 	}
 
 	public override void OnExit (){
